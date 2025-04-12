eb5d5c6f5eaa2aee84cce7f39c1b6b1cf47bdf91
abhipatel21303 <abhipatel21303@gmail.com>
2025-04-12 08:08:00 +0000

    Making UI Responsive

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,13 +2,19 @@ import { Component } from '@angular/core';
 import { RouterLink, RouterLinkActive, RouterOutlet } from '@angular/router';
 import { LoaderComponent } from './loader/loader.component';
 import { FormsModule } from '@angular/forms';
+import { FooterComponent } from './footer/footer.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, FormsModule],
+  imports: [RouterOutlet, FormsModule, FooterComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'Practice2';
+  isMenuOpen = false;
+
+  toggleMenu() {
+    this.isMenuOpen = !this.isMenuOpen;
+  }
 }
